4e6e188c427f05b8b41a4252c9c4350c4a8ec20c
sampathbabu1 <sampath.babu@zemosolabs.com>
2022-06-08 13:03:49 +0530

    Task-Items done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { ButtonComponent } from './components/button/button.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+         AppComponent,
+         TasksComponent,
+         ButtonComponent,
+         TaskItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
